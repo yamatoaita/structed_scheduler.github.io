538926ede0e2508c6ee4c068767a5ee8df1b74c3
yamatoaita <yamatoaita@gmail.com>
2024-12-15 07:43:49 +0900

    Add files via upload

---
+++ out.js
@@ -0,0 +1,26 @@
+class App{
+    constructor(){
+        /*
+        1.メールの解析
+            a.解析は過去一日分のみ（アプリ製作前、過去分は手動で）
+            b.メール差出人ごと　function製作？　または、・・・
+            c.日付(this.when) 内容(this.contents) サイト等(this.link)　タイトル(this.title)を格納
+        
+        2.Google Calenderへ記入
+            a. this.when, this.contents, this.link, this.titleを使って予定作成
+            b.通常の青じゃない、他の色に設定（視覚的に構造化する）
+            c.alerm設定は2週間と1週間前、2日前、1日前、5時間前。細かく区切る。
+        
+        3.スケジュール構造化くんHPに記載
+            a.予約一覧の場所と、課題・仕事欄を区切る
+            b.予約一覧場所にthis.when等を使って記載
+            ➡pythonアプリ　TaskSchedulerが読み取れるように工夫
+        */
+        
+    }
+
+
+
+}
+
+var app = new App();
